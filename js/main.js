79b7306794629f09f472de64966788cf1cef8ade
Sefat-Ali <115575450+Sefat-Ali@users.noreply.github.com>
2022-12-03 12:19:31 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// animationCounter.js code start
+$('#counter-block').ready(function(){
+        $('.project').animationCounter({
+          start: 0,
+          step: 10,
+          end:1240,
+          delay:30
+        });
+     $('.number-0').animationCounter({
+          start: 0,
+          step: 10,
+          end:2000,
+          delay:20
+        });
+     $('.number-2').animationCounter({
+          start: 0,
+          step: 50,
+          end:10000,
+          delay:20
+        });
+});
+// animationCounter.js code end
